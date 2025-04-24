31c659d56dd330b8255916d41901c74455b4c032
souhail <souhailtest188@gmail.com>
2025-04-24 18:00:45 +0100

    added admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Test admin',
+            'email' => 'admin@example.com',
+            'usertype'=>'admin',
+            'password'=> 'admin123',
         ]);
     }
 }
