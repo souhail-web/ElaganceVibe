baa46d01f5652bfbfa191fce5ea109aa8f814711
souhail_el <souhailtest188@gmail.com>
2025-04-24 15:32:50 +0100

    Commit de souhail modification sur web.php et ajouter controller ProductController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,6 +19,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+//Souhail est ajouté cette partie🐱‍👤
+Route::middleware(['auth','admin'])->group(function (){
+
+    Route::get('admin/dashboard',[HomeController::class, 'index']);
+    Route::get('admin/products',[ProductController::class, 'index'])->name('admin/products');
+
+});
+
 require __DIR__.'/auth.php';
 
-Route::get('admin/dashboard',[HomeController::class, 'index'])->middleware(['auth','admin']);
+//khaliw had les liens hna merci👀
+//Route::get('admin/dashboard',[HomeController::class, 'index']);
+//Route::get('admin/dashboard',[HomeController::class, 'index'])->middleware(['auth','admin']);
